466262b2cb526d9c926315a29b6a87290892371d
Sebastian Skov <dashxgraphics@gmail.com>
2022-01-05 22:41:10 +0100

    Added portfolio page, page transitions and fixed mobile load animation

--- js/app.js
+++ js/app.js
@@ -28,6 +28,11 @@ function loadAnimation() {
     opacity: 0,
     delay: 0.5,
   })
+  gsap.to('#loadlogo', {
+    duration: 1,
+    opacity: 0,
+    delay: 2,
+  })
   gsap.from(".herostagger", {
     duration: 1,
     y: 100,
@@ -74,7 +79,64 @@ gsap.from(".servicesheading", {
   duration: 1,
   opacity: 0,
   x: -100,
-  stagger: 0.1
+  stagger: 0.1,
+  delay: 1
 })
 
-setTimeout(function(){ document.getElementById('loadbg').style.display = 'none' }, 3000);
+setTimeout(function(){ document.getElementById('loadbg').style.pointerEvents = 'none' }, 3000);
+
+//var loader = document.getElementById('loader')
+
+gsap.set('#loader', {
+  scaleX: 0,
+  rotation: 10,
+  xPercent: -5,
+  yPercent: -50,
+  transformOrigin: 'left center',
+  autoAlpha: 1
+});
+
+barba.init({
+  transitions: [{
+      async leave() {
+          await loaderIn();
+      },
+      enter() {
+          loaderAway();
+      }
+  }]
+});
+
+function loaderIn() {
+  // GSAP tween to stretch the loading screen across the whole screen
+  return gsap.fromTo('#loader',
+      {
+          rotation: 10,
+          scaleX: 0,
+          xPercent: -5
+      },
+      {
+          duration: 0.8,
+          xPercent: 0,
+          scaleX: 1,
+          rotation: 0,
+          ease: 'power4.inOut',
+          transformOrigin: 'left center'
+      });
+}
+
+function loaderAway() {
+  // GSAP tween to hide loading screen
+  return gsap.to('#loader', {
+      duration: 0.8,
+      scaleX: 0,
+      xPercent: 5,
+      rotation: -10,
+      transformOrigin: 'right center',
+      ease: 'power4.inOut'
+  });
+}
+
+barba.hooks.enter(() => {
+  window.scrollTo(0, 0);
+});
